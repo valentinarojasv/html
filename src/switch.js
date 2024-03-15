e453a096a38c038ee608654137cbf9b0dfee840d
valentinarojasv <valerv.1101@gmail.com>
2024-03-15 10:59:06 -0600

    switch, version3

--- src/switch.js
+++ src/switch.js
@@ -1,38 +1,73 @@
 document.querySelector('#header').innerHTML = "Estoy leyendo el documento switch"
-var fruta
+var semana
 var mensaje
-var userInput = prompt ("ingrese el numero de la fruta")
+var userInput = prompt ("ingrese el numero del dia")
 
-function jugo_de_fresas() {
+function dia_lunes() {
     document.querySelector('#header').innerHTML = mensaje
-    document.querySelector('#header').style.color ="red";
+    document.querySelector('#header').style.color ="purple";
 }
-
-function jugo_de_naranja() {
-    mensaje = "Batir naranjas y mandarinas";
+function dia_martes() {
+    document.querySelector('#header').innerHTML = mensaje
+    document.querySelector('#header').style.color ="black";
+}
+function dia_miercoles() {
     document.querySelector('#header').innerHTML = mensaje
     document.querySelector('#header').style.color ="orange";
-
 }
-
-function servir_vaso_de_agua() {
-    mensaje = "Servir vaso con agua";
+function dia_jueves() {
+    document.querySelector('#header').innerHTML = mensaje
+    document.querySelector('#header').style.color ="yellow";
+ }
+ function dia_viernes() {
+    document.querySelector('#header').innerHTML = mensaje
+    document.querySelector('#header').style.color ="green";
+}
+function dia_sabado() {
+    document.querySelector('#header').innerHTML = mensaje
+    document.querySelector('#header').style.color ="red";
+} 
+function dia_domingo() {
+    document.querySelector('#header').innerHTML = mensaje
+    document.querySelector('#header').style.color ="pink";
+}
+function dia_invalido() {
+    mensaje = "Dia invalido";
     document.querySelector('#header').innerHTML = mensaje
     document.querySelector('#header').style.color ="blue";
   }
 
-fruta = parseInt(userInput)// con paserInt se convierte a entero
+semana = parseInt(userInput)// con paserInt se convierte a entero
 
-switch(fruta){
+switch(semana){
     case 1:
-        mensaje= "Batir fresas"
-        jugo_de_fresas()
+        mensaje= "Lunes";
+        dia_lunes()
     break
     case 2:
+        mensaje= "Martes";
+        dia_martes()
+    break
     case 3:
-        jugo_de_naranja()
+        mensaje= "Miercoles";
+        dia_miercoles()
+    break
+    case 4:
+        mensaje = "Jueves";
+        dia_jueves()
+    break
+    case 5:
+        mensaje= "Viernes";
+        dia_viernes()
+    break 
+    case 6:
+        mensaje= "Sabado";
+        dia_sabado()
+    break
+    case 7:
+        mensaje= "Domingo";
+        dia_domingo()
     break
     default:
-        servir_vaso_de_agua()
-    break    
+        dia_invalido()   
 }
