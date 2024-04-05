34f55de9f06e04e73f468effa14836801456e51a
valentinarojasv <valerv.1101@gmail.com>
2024-04-05 11:09:21 -0600

    cesta

---
+++ src/cesta.js
@@ -0,0 +1,5 @@
+document.querySelector('#header').innerHTML = "Estoy leyendo el documento Cesta.js"
+
+var cesta = ["Fresa","Pera","Cereza"]
+
+var opcion = prompt("1. Ver elementos de la cesta\n2. Agregar una fruta a la cesta\n3. Quitar una fruta de la cesta")
